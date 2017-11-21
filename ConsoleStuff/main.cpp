49ef6688fe47680903811b0eebdba6b74ef05e5b
Devon Poon Woo <devon.poonwoo@uoit.net>
2017-11-21 16:33:24 -0500

    Add project files.

---
+++ ConsoleStuff/main.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <Windows.h>
+#include <Tchar.h>
+
+HANDLE wHnd;    // Handle to write to the console.
+HANDLE rHnd;    // Handle to read from the console.
+
+void main() {
+
+	// Set up the handles for reading/writing:
+	wHnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	rHnd = GetStdHandle(STD_INPUT_HANDLE);
+
+	// Change the window title:
+	SetConsoleTitle(TEXT("Win32 Console Control Demo"));
+
+	// Set up the required window size:
+	SMALL_RECT windowSize = { 0, 0, 124, 41 };//{.Left, .Top, .Right, .Bottom}
+
+	// Change the console window size:
+	SetConsoleWindowInfo(wHnd, TRUE, &windowSize);
+
+	// Create a COORD to hold the buffer size:
+	COORD bufferSize = { 125, 42 };
+
+	// Change the internal buffer size:
+	SetConsoleScreenBufferSize(wHnd, bufferSize);
+
+	//SetConsoleDisplayMode(wHnd, CONSOLE_FULLSCREEN_MODE, &bufferSize);
+
+	system("pause");
+}
